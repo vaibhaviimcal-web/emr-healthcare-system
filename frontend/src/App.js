35db9e16aea4e26b66f97919d0536af3148611ee
vaibhaviimcal-web <vaibhav.iimcal@gmail.com>
2025-12-07 01:31:04 +0530

    Create src/App.js - main React application component

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>EMR Healthcare System</h1>
+        <p>Welcome to the Electronic Medical Records System</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
